f706cb90e4528717425d937e21dcf3e1e3bb779c
gongmengnan <gong.mengnan@ninjavan.co>
2019-04-30 11:52:25 +0800

    init

---
+++ unseal.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# Three scenarios
+# * Vault is not running yet
+# ** Just wait...
+# * Vault is running but not initialized
+# ** Exit, it needs to be initialized and unsealed manually
+# * Vault is running and initialized but sealed
+# ** Take action and unseal the vault, then exit
+# * Vault is running, initialized and unsealed
+# ** all is good, exit
+
+COUNT=1
+LIMIT=30
+export VAULT_ADDR=http://127.0.0.1:8200
+while [ 1 ]
+do
+  echo "Checking if Vault is up and running (try $COUNT)..." &> /proc/1/fd/1
+  VAULT_STATUS=$(vault status 2>&1)
+  EXIT_STATUS=$?
+  if echo "$VAULT_STATUS" | grep "server is not yet initialized"; then
+    echo "Vault not initialized.  Must be manually unsealed. Exiting..."
+    exit 0
+  elif [ $EXIT_STATUS -eq 0 ]; then
+    echo "Vault is unsealed. Exiting..."
+    exit 0
+  elif [ $EXIT_STATUS -eq 2 ]; then
+    echo "Vault Sealed.  Unsealing..."
+    vault operator unseal ${VAULT_UNSEAL_KEY}
+    exit 0
+  elif [ $COUNT -ge $LIMIT ]; then
+    # Dont know what happened... Exiting
+    echo "$VAULT_STAUS"
+    exit 1
+  else
+    # For debugging
+    echo "$VAULT_STATUS"
+  fi
+  COUNT=$((COUNT+1))
+  sleep 1
+done
