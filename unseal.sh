922fb6c7650e6ffdb6441de901243a8856c34e3a
gongmengnan <gong.mengnan@ninjavan.co>
2019-04-30 11:55:55 +0800

    update

--- unseal.sh
+++ unseal.sh
@@ -13,29 +13,34 @@
 COUNT=1
 LIMIT=30
 export VAULT_ADDR=http://127.0.0.1:8200
+
+# sleep for 5 seconds, waiting for the vault starts
+sleep 5
+
 while [ 1 ]
 do
   echo "Checking if Vault is up and running (try $COUNT)..." &> /proc/1/fd/1
   VAULT_STATUS=$(vault status 2>&1)
   EXIT_STATUS=$?
   if echo "$VAULT_STATUS" | grep "server is not yet initialized"; then
-    echo "Vault not initialized.  Must be manually unsealed. Exiting..."
+    echo "Vault not initialized.  Must be manually unsealed. Exiting..." &> /proc/1/fd/1
     exit 0
   elif [ $EXIT_STATUS -eq 0 ]; then
-    echo "Vault is unsealed. Exiting..."
+    echo "Vault is unsealed. Exiting..." &> /proc/1/fd/1
     exit 0
   elif [ $EXIT_STATUS -eq 2 ]; then
-    echo "Vault Sealed.  Unsealing..."
-    vault operator unseal ${VAULT_UNSEAL_KEY}
+    echo "Vault Sealed.  Unsealing..." &> /proc/1/fd/1
+    vault operator unseal ${VAULT_UNSEAL_KEY} &> /proc/1/fd/1
     exit 0
   elif [ $COUNT -ge $LIMIT ]; then
     # Dont know what happened... Exiting
-    echo "$VAULT_STAUS"
+    echo "$VAULT_STAUS" &> /proc/1/fd/1
     exit 1
   else
     # For debugging
-    echo "$VAULT_STATUS"
+    echo "$VAULT_STATUS" &> /proc/1/fd/1
+    ps aux &> /proc/1/fd/1
   fi
   COUNT=$((COUNT+1))
-  sleep 1
+  sleep 5
 done
